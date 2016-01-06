97a9870e36c908aa17a202950f6c72977198cc5f
Quentin ADAM <quentin.adam@waxzce.org>
2016-01-06 20:05:24 +0100

    add extra tags to the build

--- build_all_docker.sh
+++ build_all_docker.sh
@@ -12,9 +12,23 @@ do
   cat builder.sh >> generated/builder.sh
   chmod +x generated/builder.sh
   docker build --rm=false -t waxzce/rust:$RUST_VERSION .
+  echo "push this tag : $RUST_VERSION"
   docker push waxzce/rust:$RUST_VERSION
   export REAL_RUST_VERSION=`docker run waxzce/rust:$RUST_VERSION cat /rust_version | tr " " _ | tr "(" _ | tr ")" _`
   docker tag waxzce/rust:$RUST_VERSION waxzce/rust:specific-$REAL_RUST_VERSION
+  echo "push this tag : $REAL_RUST_VERSION"
   docker push waxzce/rust:specific-$REAL_RUST_VERSION
+  if [ -z "$MORE_TAGS" ]
+  then
+	   echo "no more tags"
+  else
+	   echo "now theses tags $MORE_TAGS"
+     for TAG in `echo $MORE_TAGS | tr "," "\n"`
+     do
+       echo "push this tag : $TAG"
+       docker tag waxzce/rust:$RUST_VERSION waxzce/rust:$TAG
+       docker push waxzce/rust:$TAG
+     done
+  fi
   rm generated/builder.sh
 done
