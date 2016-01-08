602adebf76e5fb2664089ea3b0108c385bc7d68e
Quentin ADAM <quentin.adam@waxzce.org>
2016-01-08 15:48:19 +0100

    docker file builder

--- build_all_docker.sh
+++ build_all_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "build Dockerfile"
 
+sed "s;££DATE££;$(date +%D);" template_Dockerfile > Dockerfile
 
 for version in `ls versions/*`
 do
@@ -13,15 +15,15 @@ do
   chmod +x generated/builder.sh
   docker build --rm=false -t waxzce/rust-$RUST_VERSION .
   echo "push this tag : $RUST_VERSION"
-  docker push waxzce/rust-$RUST_VERSION
+  docker push -f waxzce/rust-$RUST_VERSION
   docker tag waxzce/rust-$RUST_VERSION waxzce/rust:$RUST_VERSION
-  docker push waxzce/rust:$RUST_VERSION
+  docker push -f waxzce/rust:$RUST_VERSION
   export REAL_RUST_VERSION=`docker run waxzce/rust:$RUST_VERSION cat /rust_version | tr " " _ | tr "(" _ | tr ")" _`
   docker tag waxzce/rust:$RUST_VERSION waxzce/rust:specific-$REAL_RUST_VERSION
   echo "push this tag : $REAL_RUST_VERSION"
-  docker push waxzce/rust:specific-$REAL_RUST_VERSION
+  docker push -f waxzce/rust:specific-$REAL_RUST_VERSION
   docker tag waxzce/rust-$RUST_VERSION waxzce/rust-$RUST_VERSION:specific-$REAL_RUST_VERSION
-  docker push waxzce/rust-$RUST_VERSION:specific-$REAL_RUST_VERSION
+  docker push -f waxzce/rust-$RUST_VERSION:specific-$REAL_RUST_VERSION
   if [ -z "$MORE_TAGS" ]
   then
 	   echo "no more tags"
@@ -31,7 +33,7 @@ do
      do
        echo "push this tag : $TAG"
        docker tag waxzce/rust:$RUST_VERSION waxzce/rust:$TAG
-       docker push waxzce/rust:$TAG
+       docker push -f waxzce/rust:$TAG
      done
   fi
   rm generated/builder.sh
